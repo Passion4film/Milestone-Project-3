8ab9ba4cb26969f07ad02430b6f8111e6b627c3d
Passion4film <passion4film@hotmail.co.uk>
2021-03-17 12:49:35 +0000

    Edit a recipe into the database functionality.

--- app.py
+++ app.py
@@ -127,6 +127,22 @@ def add_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
+    if request.method == "POST":
+        is_vegan = "on" if request.form.get("is_vegan") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients_measurements":
+                request.form.getlist("ingredients_measurements"),
+            "preparation_steps": request.form.get("preparation_steps"),
+            "time": request.form.get("time"),
+            "is_vegan": is_vegan,
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
+        flash("Recipe Successfully Edited")
+        return redirect(url_for("get_recipes"))
+        
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)

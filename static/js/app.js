5e371c73c0fd08987b304e0305062ebad8216d8a
Passion4film <passion4film@hotmail.co.uk>
2021-03-29 15:55:20 +0000

    adding a new step to the preparation steps the same as the ingredients on add a recipe page.

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,26 @@ var ingredientCount = 1;
             });
         });
     });
+
+/*
+    function for adding new recipe preparation steps 
+*/
+var stepCount = 1;
+    document.getElementById('addStep').addEventListener('click', function(){
+        let nextId = 'next'+ stepCount + 1;
+        const labelText = 'Next Step'
+        const removeId = 'remove'+ stepCount;
+        stepCount = stepCount + 1;
+        const newInput = '<div class="input-field col s12"> <i class="fas fa-clipboard-list prefix deep-purple-text text-darken-3 text-shadow"></i> <textarea id="'+ labelText + '" name="ingredients_measurements" minlength="5" maxlength="300" class="materialize-textarea validate" required="" spellcheck="false"></textarea> <label for="'+ labelText + '">'+ labelText + '</label><i class="fas fa-trash-alt deep-purple-text text-darken-3 text-shadow removeInput" id="'+ removeId + '"></i></div>';
+        newDiv = document.createElement("div")
+        newDiv.id = nextId;
+        document.getElementById('extraSteps').appendChild(newDiv);
+        document.getElementById(nextId).innerHTML = newInput;
+
+        var elems = document.querySelectorAll(".removeInput");
+        [].forEach.call(elems, function(el) {
+            el.addEventListener('click', function(event){
+                event.target.parentElement.remove();
+            });
+        });
+    });
